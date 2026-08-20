3348bd1b37df5da3f0666b80d114fdd4085d6035
ydh1121 <ydh1121@naver.com>
2026-08-20 19:03:23 +0900

    Correct registry escaping while loading admin shell

--- public/assets/js/blocks/block-registry.js
+++ public/assets/js/blocks/block-registry.js
@@ -5,7 +5,7 @@
 
   function escapeHtml(value=''){
     return String(value??'').replace(/[&<>"']/g,char=>({
-      '&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot',"'":'&#039;'
+      '&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;',"'":'&#039;'
     }[char]));
   }
 
